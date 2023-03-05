9e7afa63a03e1962317b115ed0376ab800b858ce
ayato-shitomi <sw.172.135.9.xxx.l62118102@gmail.com>
2023-03-05 00:03:24 -0500

    basic_picture_forensic on Sun Mar  5 12:03:24 AM EST 2023

--- basic_picture_forensic/main.py
+++ basic_picture_forensic/main.py
@@ -5,7 +5,7 @@ import base64
 
 def	run_cmd(cmd):
 	print(cmd)
-	#os.system(cmd)
+	os.system(cmd)
 
 i = input("画像ファイルを選択:	")
 
@@ -14,9 +14,9 @@ path = (str(datetime.datetime.now()) + "_" + i).replace(".","-").replace(":","-"
 print()
 cmd = "mkdir ./" + path
 run_cmd(cmd)
-cmd = "strings " + i + " >> ./" + path + "/strings.txt"
+cmd = "strings " + i + " > ./" + path + "/strings.txt"
 run_cmd(cmd)
-cmd = "exiftool " + i + " >> ./" + path + "/exiftool.txt"
+cmd = "exiftool " + i + " > ./" + path + "/exiftool.txt"
 run_cmd(cmd)
 print()
 
@@ -28,33 +28,33 @@ def ch_flag(s1, s2, mode):
 		if mode == 1:
 			print("\033[1m", s1, "\033[0m\t", s2)
 
-def	from_base64_exif(path):
+def	from_base64_exif(p):
 	print("=== plain text and base64 decode on exif command ===")
-	with open(path) as fi:
+	with open(p) as fi:
 		n = 0
 		for i in fi:
 			tmp = i.split(": ")[1].replace("\n", "")
-			ch_flag(path + ":" + str(n), tmp, 0)
+			ch_flag(p + ":" + str(n), tmp, 0)
 			try:
-				ch_flag(path + ":" + str(n) + " '" + tmp + "' -> ", base64.b64decode(tmp), 1)
+				ch_flag(p + ":" + str(n) + " '" + tmp + "' -> ", base64.b64decode(tmp), 1)
 			except:
 				pass
 			n = n + 1
 	print()
 
-def from_base64_string(path):
+def from_base64_string(p):
 	print("=== plain text and base64 on strings command ===")
-	with open(path) as fi:
+	with open(p) as fi:
 		for i in fi:
 			n = 0
 			tmp = i.replace("\n", "")
-			ch_flag(path + ":" + str(n), tmp, 0)
+			ch_flag(p + ":" + str(n), tmp, 0)
 			try:
-				ch_flag(path + ":" + str(n) + " '" + tmp + "' -> ", base64.b64decode(tmp), 0)
+				ch_flag(p + ":" + str(n) + " '" + tmp + "' -> ", base64.b64decode(tmp), 0)
 			except:
 				pass
 			n = n + 1
 	print()
 
-from_base64_string("./2023-03-04_23-06-01-592408_cat-jpg/strings.txt")
-from_base64_exif("./2023-03-04_23-06-01-592408_cat-jpg/exiftool.txt")
+from_base64_string("./" + path + "/strings.txt")
+from_base64_exif("./" + path + "/exiftool.txt")
